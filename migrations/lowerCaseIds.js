df0d0573221b9c83ac101c8c9d08e670dc261340
Will Murphy <william@datatitian.com>
2019-09-25 21:59:39 -0500

    also update ids in streams

--- migrations/lowerCaseIds.js
+++ migrations/lowerCaseIds.js
@@ -2,3 +2,11 @@ db.objects.find({type: "Group"}).forEach(function(d){
   d.id = d.id.toLowerCase();
   db.objects.save(d);
 });
+db.streams.find({"_meta._target": {$exists: true}}).forEach(function(d){
+  d._meta._target = d._meta._target.toLowerCase();
+  db.objects.save(d);
+});
+db.streams.find({actor: /gup\.pe/}).forEach(function(d){
+  d.actor = d.actor.toLowerCase();
+  db.streams.save(d);
+});
